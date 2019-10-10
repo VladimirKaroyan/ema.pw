90fef04616756083e6352cd36c00f11e1cbfc168
vladimirkaroyan <vladimir.kyan@gmail.com>
2019-10-10 19:00:48 +0400

    Change style of index page, add error catching

--- app.js
+++ app.js
@@ -74,4 +74,13 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
+process
+    .on('unhandledRejection', (reason, p) => {
+        console.error(reason, 'Unhandled Rejection at Promise ', p);
+    })
+    .on('uncaughtException', err => {
+        console.error(err, 'Uncaught Exception thrown');
+        process.exit(1);
+    });
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ let shopservice = require('../services/database');
 router.get('/', function (req, res, next) {
     shopservice.getAllProducts().then(function (data) {
         if (data instanceof Error) throw res.render('error', {error: data});
+        let sortedData = {};
+        data.map((row) => {
+            let line = row.productLine;
+            (sortedData[line] !== undefined) ? sortedData[line].push(row) : sortedData[line] = [row];
+        });
         res.render('index', {
             title: 'Express',
-            data: data,
+            sortData: sortedData,
             user: req.user
         });
     });
